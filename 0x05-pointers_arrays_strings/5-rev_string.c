3da7a5f30f877ea3d56c2975979acf12d87897ba
root <rozenhertfatima>
2023-06-26 11:56:16 -0700

    5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,4 +5,24 @@
  *Return: No return.
  */
 void rev_string(char *s)
-{
+{{
+    int i = 0;
+    int b = 0;
+    int c;
+
+    while (s[b] != '\0')
+    {
+        b++;
+    }
+
+    while (i < b / 2)
+    {
+        c = s[i];
+        s[i] = s[b - i - 1];
+        s[b - i - 1] = c;
+        i++;
+    }
+}
+	
+    }
+
