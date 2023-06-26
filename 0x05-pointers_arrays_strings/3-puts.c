335fe719f08550c0ac50ec4dc167d7ce6c48cb4e
root <rozenhertfatima>
2023-06-26 11:08:24 -0700

    3-puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,11 +6,11 @@
  */
 void _puts(char *str)
 {
-	 while (*str != '\0')
-	 {
-            _putchar(*str);
-            str++;
-        }
-        _putchar('\n');
-    }
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
 
