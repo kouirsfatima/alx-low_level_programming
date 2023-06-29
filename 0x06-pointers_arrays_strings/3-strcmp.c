a2dabbd108b403d5b1bac49067cba66136102ca1
root <rozenhertfatima>
2023-06-29 02:13:29 -0700

    2-strncpy

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,16 @@
 int _strcmp(char *s1, char *s2)
 
 {
-	int i;
-	while(s1[i] != '\0' && s2[i] != '\0')
+	
+	int i = 0,n = 0;
+
+	while (n == 0)
 	{
-		if(s1[i] != s2[i])
-		{
-			return 1;
-		}
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		n = *(s1 + i) - *(s2 + i);
 		i++;
 	}
-	return 0;
+
+	return (n);
 }
