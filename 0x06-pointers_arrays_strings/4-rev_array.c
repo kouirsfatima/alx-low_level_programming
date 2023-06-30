cc7dca21653f268e2d599147a4ef3b771f68d37c
root <rozenhertfatima>
2023-06-30 02:44:08 -0700

    4-rev

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,12 +9,14 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, tmp;
+	 int i = 0;
+    int temp = 0;
 
-    for (i = 0; i < n / 2; i++)
+    while (i < n / 2)
 	{
-        tmp = a[i];
+        temp = a[i];
         a[i] = a[n - i - 1];
-        a[n - i - 1] = tmp;
+        a[n - i - 1] = temp;
+        i++;
     }
 }
