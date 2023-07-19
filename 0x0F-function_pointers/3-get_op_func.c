76f984df851e497f11ba5337963e84f9b22533ae
root <rozenhertfatima>
2023-07-19 02:20:15 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,12 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct function to perform
+ * the operation asked by the user.
+ * @s: char operator.
+ *
+ * Return: pointer to the function that corresponds to the operator.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+
