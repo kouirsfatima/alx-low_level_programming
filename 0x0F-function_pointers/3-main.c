dc84bc5ad6f81ff17ffb91e18b172b0161700c96
root <rozenhertfatima>
2023-07-19 14:57:10 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,47 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "3-calc.h"
-
 /**
- * main - check the code for ALX students.
- * @argc: argument count.
- * @argv: argument vector.
- *
+ * main - check the code
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings representing the arguments
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	int (*op_func)(int, int);
-
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	if (argv[2][1])
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	op_func = get_op_func(argv[2]);
-	if (op_func == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
-	printf("%d\n", op_func(num1, num2));
-	return (0);
+int res = 0, n1, n2;
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
 }
+if (strcmp(argv[2], "+") != 0 && strcmp(argv[2], "-") != 0 &&
+strcmp(argv[2], "*") != 0 && strcmp(argv[2], "/") &&
+strcmp(argv[2], "%"))
+{
+printf("Error\n");
+exit(99);
+}
+if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) &&
+strcmp(argv[3], "0") == 0)
+{
+printf("Error\n");
+exit(100);
+}
+n1 = atoi(argv[1]);
+n2 = atoi(argv[3]);
 
+res = (*get_op_func(argv[2]))(n1, n2);
+printf("%d\n", res);
 
-
-
-
-
-
+return (0);
+}
 
