7eb46985a482db48fff26da0d89ffd37b766aa74
root <rozenhertfatima>
2023-07-19 02:58:50 -0700

    modified:   2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,17 +15,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-    if (cmp && array)
-    {
-        if (size <= 0)
-            return -1;
+	if (cmp && array)
+	{
+		if (size <= 0)
+			return (-1);
 
-        for (i = 0; i < size; i++)
-        {
-            if (cmp(array[i]))
-                return i;
-        }
-    }
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
 
-    return -1;
+	return (-1);
 }
