80453c7dcc7c85b6121b073ecb203187cc607f61
root <rozenhertfatima>
2023-07-27 03:11:27 -0700

    fixed error

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,13 +7,13 @@
 
 size_t list_len(const list_t *h)
 {
-
     size_t ptr = 0;
 
     while (h != NULL)
-	    h = h->next;
-	ptr++;
+    {
+        ptr++;
+        h = h->next;
+    }
 
     return (ptr);
 }
-
