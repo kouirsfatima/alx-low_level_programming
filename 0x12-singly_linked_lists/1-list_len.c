fa0363c5d647fa5e3e114427def321fc86b8fceb
root <rozenhertfatima>
2023-07-27 03:12:37 -0700

    fixed error

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,13 +7,13 @@
 
 size_t list_len(const list_t *h)
 {
-    size_t ptr = 0;
+	size_t ptr = 0;
 
-    while (h != NULL)
-    {
-        ptr++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		ptr++;
+		h = h->next;
+	}
 
-    return (ptr);
+	return (ptr);
 }
