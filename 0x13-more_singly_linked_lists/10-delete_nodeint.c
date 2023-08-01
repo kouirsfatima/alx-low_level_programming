eccab644a3f1734f06855ca51a45f2f35be19b0c
root <rozenhertfatima>
2023-08-01 04:35:25 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
 	listint_t *prev, *current;
-if (head == NULL || *head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	current = *head;
@@ -25,11 +25,15 @@ if (head == NULL || *head == NULL)
 	}
 
 	prev = current;
+	if (current != NULL)
+		return (1);
 	for (i = 0;   i < index; i++)
-	{
-		prev = current;
-		current = current->next;
-	}
+		if (current != NULL)
+
+		{
+			prev = current;
+			current = current->next;
+		}
 
 	if (current == NULL)
 		return (-1);
