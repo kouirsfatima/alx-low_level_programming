56cd71078a1bfe1393ac23be97e08167926abcc0
root <rozenhertfatima>
2023-08-03 06:38:01 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,27 +9,22 @@
  */
 void print_binary(unsigned long int n)
 {
-	int mask = 1;
-	int result = 0;
+	int i;
+	unsigned int j, k, a;
 
-	if (n > 0)
+	for (i = sizeof(n) * 8 - 1; i >= 0; i--)
 	{
-		while (mask <= (int)n)
-		{
-			if (n & mask)
-			{
-				result = (result << 1) | 1;
-			}
-			else
-			{
-				result <<= 1;
-			}
+		j = i;
+		a = 1 << j;
+		k = n & a;
 
-			mask <<= 1;
+		if (k == 0)
+		{
+			_putchar('0');
+		}
+		else
+		{
+			_putchar('1');
 		}
 	}
-
-	if (result == 0)
-
-		_putchar('0');
 }
