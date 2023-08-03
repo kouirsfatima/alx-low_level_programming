45a6e16a0d588ae238ac2dcb3697b0ca1dc5d2fb
root <rozenhertfatima>
2023-08-03 05:10:12 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,25 +11,30 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-
-	int slen = strlen(b);
 	unsigned int total = 0;
-	unsigned int devcal = 1;
-	int i;
+	int len = 0, a = 1;
 
 	if (b == NULL)
 		return (0);
 
-	for (i = slen - 1; i >= 0; i--)
+	while (b[len])
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
-
-		if (b[i] == '1')
-			total += devcal;
-
-		devcal *= 2;
+		len++;
 	}
+	len--;
+
+	if (b[0] == '1' && b[1] == '\0')
+		return (1);
+
+	do {
+		if (b[len] == '1')
+			total += a;
+		a *= 2;
+		len--;
+
+	}	while (len >= 0);
 
 	return (total);
 }
