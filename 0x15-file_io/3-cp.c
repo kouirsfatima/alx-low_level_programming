8b141cc1343e3fcc7913e7bdd2d36afb008538a2
root <rozenhertfatima>
2023-08-07 05:26:21 -0700

    modified:   3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,26 @@
 #include "main.h"
 /**
+ * check_file- checks if files can be opened.
+ * @fd_from: file_from.
+ * @fd_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+int check_file(int fd_from, int fd_to, char *argv[])
+{
+        if (fd_from == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+                exit(98);
+        }
+        if (fd_to == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+                exit(99);
+        }
+        return (0);
+}
+/**
  * main - check the code for ALX students.
  * @argc: number of arguments.
  * @argv: arguments vector.
