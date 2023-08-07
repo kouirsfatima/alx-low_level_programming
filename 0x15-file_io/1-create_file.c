a12354bb485c9e992f0cd60cf81e0703bf00e2f8
root <rozenhertfatima>
2023-08-07 04:54:08 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * create_file - creates a file
  * @filename: filename.
@@ -14,12 +13,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0640);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
+	if (text_content != NULL)
 	{
 		while (text_content[ptr])
 			ptr++;
