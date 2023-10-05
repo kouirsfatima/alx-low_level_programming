4215460d62f004d965fb83f4b98a34a73c524077
kouirsfatima <fatimaberline535@gmail.com>
2023-10-05 17:20:17 +0100

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,12 +9,14 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 {
-    hash_node_t *new = NULL;
+    hash_node_t *new = NULL, *tmp = NULL;
     unsigned long int = idx;
+    
 
 	if (key == NULL || *key ==NULL)
 		return(0);
     idx = key_index((const char *)key, ht->size);
+
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return(0);
@@ -22,12 +24,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 	new->key = strdup(key);
 	new-> value = strdup(value);
 	new->next = NULL;
+
+    tmp = ht->array[idx]
+    while ()
+    {
+        /* code */
+    }
     
-
-
-
-
-
-
 
 }
