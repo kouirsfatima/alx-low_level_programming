faaf4d0c78b5eef4da01a3f59a36bbd50feb2f19
kouirsfatima <fatimaberline535@gmail.com>
2023-10-05 17:30:36 +0100

    fix task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,7 +3,6 @@
 /**
  * key_index - function that generates a distributed index
  * according to a given hash
- *
  * @key: key passed
  * @size: size of the hash tables
  * Return: key index
@@ -12,8 +11,8 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int idex = 0;
 
-	if (size == 0 || key == NULL)
-		return (NULL);
-	idex = hash_djb(key) % size;
+	if (size == 0 || key == NULL || key[0] == '\0')
+		return (0);
+	idex = hash_djb2(key) % size;
 	return (idex);
 }
