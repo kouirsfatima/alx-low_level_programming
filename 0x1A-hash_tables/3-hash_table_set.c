f62bf0753d8ba494943730a0b87907d780192823
kouirsfatima <fatimaberline535@gmail.com>
2023-10-06 13:12:54 +0100

    task 5

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,40 +1,37 @@
-#include"hash_tables.h"
+#include "hash_tables.h"
+
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: The hash table
- * @key: The key of the new element
- * @value: The value of the new element
- *
- * Return: 1 on success, 0 on failure
+ * hash_table_set - adds an element to the hash table.
+ * @ht: Hash table
+ * @key: The key
+ * @value: The value of the key
+ * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new = NULL, *tmp = NULL;
-	unsigned long int idx = 0;
+	hash_node_t *new_node, *current;
+	unsigned long int index = 0;
 
-	if (ht == NULL || !key || !key[0])
+	if (!ht || !key || key[0] == '\0')
 		return (0);
-	idx = key_index((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-		return (0);
-
-	new->key = strdup(key);
-	new->value = strdup(value);
-	new->next = NULL;
-
-	tmp = ht->array[idx];
-	while (tmp)
+	current = ht->array[index];
+	while (current)
 	{
-		if (strncmp(key, tmp->key, strlen(tmp->key) + 1) == 0)
+		if (strncmp(key, current->key, strlen(current->key) + 1) == 0)
 		{
-			strcpy(tmp->value, value);
+			free(current->value);
+			current->value = strdup(value);
 			return (1);
 		}
-		tmp = tmp->next;
+		current = current->next;
 	}
-	new->next = ht->array[idx];
-	ht->array[idx] = new;
+	new_node = malloc(sizeof(hash_node_t));
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
 	return (1);
 }
